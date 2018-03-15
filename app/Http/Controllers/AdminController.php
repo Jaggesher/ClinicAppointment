a30f8c5cf73dc470697856268ea1ab56025de32a
Jaggesher Mondal <jaggesher14@gmail.com>
2018-03-16 00:10:33 +0600

    3 basic Controller added

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jaggesher
+ * Date: 16-Mar-18
+ * Time: 12:09 AM
+ */
+
+namespace App\Http\Controllers;
+
+
+class AdminController extends Controller
+{
+
+}
---
+++ app/Http/Controllers/DoctorController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jaggesher
+ * Date: 16-Mar-18
+ * Time: 12:08 AM
+ */
+
+namespace App\Http\Controllers;
+
+
+class DoctorController extends Controller
+{
+
+}
