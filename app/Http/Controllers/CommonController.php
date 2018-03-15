a398684bda40626452794398020290703e310130
Jaggesher Mondal <jaggesher14@gmail.com>
2018-03-16 00:11:33 +0600

    Common Controller added

---
+++ app/Http/Controllers/CommonController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jaggesher
+ * Date: 16-Mar-18
+ * Time: 12:10 AM
+ */
+
+namespace App\Http\Controllers;
+
+
+class CommonController extends Controller
+{
+
+}
