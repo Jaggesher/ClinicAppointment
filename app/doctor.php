d83d2f733c8019feaedda9e544228cf26ce50b0b
Jaggesher Mondal <jaggesher14@gmail.com>
2018-03-17 03:18:54 +0600

    Doctor and patient models are ready for auth

--- app/doctor.php
+++ app/doctor.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class doctor extends Model
 {
-    //
+    use Notifiable;
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
 }
