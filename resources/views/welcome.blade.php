ffc739924c05a7c50a25a4c2048026482d788cb5
jaggesher <jaggesher14@gmail.com>
2018-07-02 18:42:19 +0600

    Home Updated

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,12 +14,12 @@
 <div class="container">
   <br>
   <br>
-    <h1 class="text-center" style="color: white;"> <b> WELCOME</b></h1>
+    <h1 class="text-center" style="color: white;"> <b> WELCOME TO DOCTOR APPOINTMENT SYSTEM</b></h1>
     <hr>
     <br>
     
-    <p class="text-center" style="font-size: 20px; color: #FAFAFA">
-      this is some text 
+    <p class="text-center" style="font-size: 40px; color: #FAFAFA">
+      <marquee>Find and Book a Doctor Appointment right now</marquee>
     </p>
 </div>
 
